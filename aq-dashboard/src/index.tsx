aa4a2894b76c5c7aba30859c68b302f709e30442
LCSOGthb <185141600+LCSOGthb@users.noreply.github.com>
2025-09-02 00:07:30 +0800

    Add Sentry (sentry.io) supports

--- aq-dashboard/src/index.tsx
+++ aq-dashboard/src/index.tsx
@@ -3,17 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+Sentry.init({
+  dsn: "https://ea94094f4f9d059f512a93c7d8ba3aca@o4509151884148736.ingest.us.sentry.io/4509826175401984",
+  sendDefaultPii: true
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const container = document.getElementById("app");
+
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error('No element with id "app" found in HTML.');
+}
+
 reportWebVitals();
